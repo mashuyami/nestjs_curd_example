a51c53d80db5f4ee0d9f69f8ea76c26a0e8a1663
mashuyami <https://github.com/mashuyami>
2023-09-09 17:51:51 +0700

    curd +instruct

---
+++ src/cliente/dto/create-cliente.dto.ts
@@ -0,0 +1,20 @@
+import { IsBoolean, IsString } from 'class-validator';
+export class CreateClienteDto {
+    @IsString()
+nome: string;
+
+@IsString()
+email: string;
+
+@IsString()
+cpf: string;
+
+@IsString()
+fone: string;
+
+@IsString()
+celular: string;
+
+@IsBoolean()
+status: boolean;
+}
